f75bdbc42b2264ddeda9f633f7c8f62fc2a1321d
druloloy <druloloy@proton.me>
2022-12-13 02:00:28 +0800

    updated cors

--- cors.config.js
+++ cors.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    origin: ['https://qrious-admin.netlify.app'],
+    origin: ['https://qrious-admin.netlify.app', 'http://localhost:3000'],
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
 };
